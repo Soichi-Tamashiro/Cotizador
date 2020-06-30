c9a1568a832c1a48b43b8388ecc9edd2339ff458
Soichi Tamashiro <wyvernys@live.com>
2020-06-30 18:36:43 -0500

    .gitignore fix

--- screens/.buildozer/android/platform/build-armeabi-v7a/dists/Cotizador_RAS__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/music_mad.o.d
+++
@@ -1,2 +0,0 @@
-/home/soichi/Documents/KivyMD_proyects/Cotizador/screens/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/music_mad.o: \
-  /home/soichi/Documents/KivyMD_proyects/Cotizador/screens/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_mixer/music_mad.c
